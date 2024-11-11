2ebf8667b5e3db11b9586986851d75765aa2eb48
faye <faye_1225@163.com>
2024-11-11 17:05:35 +0800

    add:test

---
+++ lib/test.ts
@@ -0,0 +1,80 @@
+
+import fs from 'fs';
+import csv from 'csv-parser';
+import path from 'path';
+import "dotenv/config"
+import { Client } from 'pg';
+
+const db = new Client({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+function parseDate(dateString: string): string {
+  const parts = dateString.split('/');
+  if (parts.length === 3) {
+    const day = parts[0].padStart(2, '0');
+    const month = parts[1].padStart(2, '0');
+    const year = parts[2];
+    return `${year}-${month}-${day}`;
+  }
+  console.warn(`Could not parse date: ${dateString}`);
+  throw Error();
+}
+
+export async function seed() {
+  await db.connect();
+  console.log('111')
+  const createTable = await db.sql`
+    CREATE TABLE IF NOT EXISTS unicorns (
+      id SERIAL PRIMARY KEY,
+      company VARCHAR(255) NOT NULL UNIQUE,
+      valuation DECIMAL(10, 2) NOT NULL,
+      date_joined DATE,
+      country VARCHAR(255) NOT NULL,
+      city VARCHAR(255) NOT NULL,
+      industry VARCHAR(255) NOT NULL,
+      select_investors TEXT NOT NULL
+    );
+  `;
+
+  console.log(`Created "unicorns" table`);
+
+  const results: any[] = [];
+  const csvFilePath = path.join(process.cwd(), 'unicorns.csv');
+console.log(csvFilePath)
+  await new Promise((resolve, reject) => {
+    fs.createReadStream(csvFilePath)
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', resolve)
+      .on('error', reject);
+  });
+
+  for (const row of results) {
+    const formattedDate = parseDate(row['Date Joined']);
+
+    await db.sql`
+      INSERT INTO unicorns (company, valuation, date_joined, country, city, industry, select_investors)
+      VALUES (
+        ${row.Company},
+        ${parseFloat(row['Valuation ($B)'].replace('$', '').replace(',', ''))},
+        ${formattedDate},
+        ${row.Country},
+        ${row.City},
+        ${row.Industry},
+        ${row['Select Investors']}
+      )
+      ON CONFLICT (company) DO NOTHING;
+    `;
+  }
+
+  console.log(`Seeded ${results.length} unicorns`);
+
+  return {
+    createTable,
+    unicorns: results,
+  };
+}
+
+
+seed().catch(console.error);
